aa103b7f7379d2d3abc77f44e9d864cff48614a0
Eugene <tashoglo00@mail.ru>
2023-11-15 02:44:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/list/GrandPeople.kt
@@ -0,0 +1,11 @@
+class Grandparent {
+    var key: String? = null
+    var address: String? = null
+    var completed: Boolean = false
+    var description: String? = null
+    var img: String? = null
+    var name: String? = null
+
+    constructor() {
+    }
+}
